68c7032e6fd09226819ebea0918e728cdaf27ce9
Doyle Hunter <meaculpa@charter.net>
2011-07-27 18:37:16 -0500

    20110727

--- BackofficeDesktop/BackofficeDesktop/ToolboxViewModel.cs
+++ BackofficeDesktop/BackofficeDesktop/ToolboxViewModel.cs
@@ -12,21 +12,44 @@ namespace BackofficeDesktop
 	{
 		private string _panelCaption = "Backoffice Tools";
 		private System.Windows.Visibility _panelVisibility = System.Windows.Visibility.Visible;
-		private ObservableCollection<PanelItem> _panelItems;
+		private ObservableCollection<BackofficeToolGroup> _toolGroups;
 		private bool _isPinned = true;
+		private int _fontSize = 16;
 
 		public ToolboxViewModel()
 		{
-			_panelItems = new ObservableCollection<PanelItem>();
-			_panelItems.Add(new PanelItem("Financials"));
-			_panelItems.Add(new PanelItem("Labor"));
-			_panelItems.Add(new PanelItem("Tools"));
+			_toolGroups = new ObservableCollection<BackofficeToolGroup>();
+			BackofficeToolGroup group = new BackofficeToolGroup("Financials", "Financial Applications", "1");
+			_toolGroups.Add(group);
+
+			group = new BackofficeToolGroup("Labor", "Labor Applications", "2");
+			_toolGroups.Add(group);
+
+			group = new BackofficeToolGroup("Tools", "Tools", "3");
+			_toolGroups.Add(group);
+		}
+
+		public void ToolGroupLabelMouseEnter()
+		{
+			System.Windows.MessageBox.Show("Hello");
+		}
+
+		public int FontSize
+		{
+			get
+			{
+				return _fontSize;
+			}
+			set
+			{
+				_fontSize = value;
+			}
 		}
 
 		public void CollapsedPanelButtonMouseEnter()
 		{
 			_panelVisibility = System.Windows.Visibility.Visible;
-			NotifyOfPropertyChange("PanelVisibility");
+			NotifyOfPropertyChange("PanelVisible");
 		}
 
 		public void CollapsedPanelButtonMouseLeave()
@@ -35,13 +58,14 @@ namespace BackofficeDesktop
 			{
 				_panelVisibility = System.Windows.Visibility.Collapsed;
 			}
-			NotifyOfPropertyChange("PanelVisibility");
+			NotifyOfPropertyChange("PanelVisible");
 		}
 
 		public void FunctionPanelMouseEnter()
 		{
+			Console.WriteLine("FunctionPanelMouseEnter");
 			_panelVisibility = System.Windows.Visibility.Visible;
-			NotifyOfPropertyChange("PanelVisibility");
+			NotifyOfPropertyChange("PanelVisible");
 		}
 
 		public void FunctionPanelMouseLeave()
@@ -50,7 +74,7 @@ namespace BackofficeDesktop
 			{
 				_panelVisibility = System.Windows.Visibility.Collapsed;
 			}
-			NotifyOfPropertyChange("PanelVisibility");
+			NotifyOfPropertyChange("PanelVisible");
 		}
 
 		public string PanelCaption
@@ -83,11 +107,27 @@ namespace BackofficeDesktop
 					_panelVisibility = System.Windows.Visibility.Collapsed;
 				}
 				NotifyOfPropertyChange("IsPinned");
-				NotifyOfPropertyChange("PanelVisibility");
+				NotifyOfPropertyChange("PanelVisible");
+				NotifyOfPropertyChange("PanelCollapsed");
 			}
 		}
 
-		public System.Windows.Visibility PanelVisibility
+		public System.Windows.Visibility PanelCollapsed
+		{
+			get
+			{
+				if (_panelVisibility == System.Windows.Visibility.Collapsed)
+				{
+					return System.Windows.Visibility.Visible;
+				}
+				else
+				{
+					return System.Windows.Visibility.Collapsed;
+				}
+			}
+		}
+
+		public System.Windows.Visibility PanelVisible
 		{
 			get
 			{
@@ -99,32 +139,108 @@ namespace BackofficeDesktop
 			}
 		}
 
-		public ObservableCollection<PanelItem> PanelItems
+		public ObservableCollection<BackofficeToolGroup> ToolGroups
 		{
 			get
 			{
-				return _panelItems;
+				return _toolGroups;
 			}
 			set
 			{
-				_panelItems = value;
+				_toolGroups = value;
 			}
 		}
 	}
 
-	public class PanelItem
+	public class BackofficeToolGroup
+	{
+		private string _caption;
+		private string _description;
+		private ObservableCollection<BackofficeTool> _tools;
+		private ObservableCollection<BackofficeToolGroup> _toolGroups;
+		private string _iconId;
+
+		public BackofficeToolGroup(string caption, string description, string iconId)
+		{
+			_caption = caption;
+			_description = description;
+			_iconId = iconId;
+		}
+
+		public string Caption
+		{
+			get
+			{
+				return _caption;
+			}
+			set
+			{
+				_caption = value;
+			}
+		}
+
+		public string Description
+		{
+			get
+			{
+				return _description;
+			}
+			set
+			{
+				_description = value;
+			}
+		}
+
+		public ObservableCollection<BackofficeTool> Tools
+		{
+			get
+			{
+				return _tools;
+			}
+			set
+			{
+				_tools = value;
+			}
+		}
+
+		public ObservableCollection<BackofficeToolGroup> ToolGroups
+		{
+			get
+			{
+				return _toolGroups;
+			}
+			set
+			{
+				_toolGroups = value;
+			}
+		}
+
+		public string IconId
+		{
+			get
+			{
+				return _iconId;
+			}
+			set
+			{
+				_iconId = value;
+			}
+		}
+	}
+
+	public class BackofficeTool
 	{
 		private string _caption;
 		private string _description;
 		private string _commandLine;
 		private string _iconId;
 
-		public PanelItem(string caption) //, string description, string commandLine, string iconId)
+		public BackofficeTool(string caption, string description, string commandLine, string iconId)
 		{
 			_caption = caption;
-			//_description = description;
-			//_commandLine = commandLine;
-			//_iconId = iconId;
+			_description = description;
+			_commandLine = commandLine;
+			_iconId = iconId;
 		}
 
 		public string Caption
